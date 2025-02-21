acd2327e839d2faa2811436decba07366b71f8a7
Jason Wang <jasonw123451@gmail.com>
2025-02-21 13:19:39 -0800

    modifying gstreamer recording pipelines to show video feed while recording

--- cam/cams_recording_left.sh
+++ cam/cams_recording_left.sh
@@ -7,7 +7,9 @@ mkdir -p ./"$dirname"
 cd ./"$dirname"
 
 gst-launch-1.0 -e rtspsrc location=rtsp://192.168.0.250:554/h264 ! rtph264depay ! h264parse ! nvv4l2decoder enable-max-performance=1 ! \
-               nvvidconv ! 'video/x-raw(memory:NVMM), format=RGBA' ! \
+               nvvidconv ! tee name=t \
+               t. ! queue ! autovideosink sync=false \ 
+               t. ! queue ! 'video/x-raw(memory:NVMM), format=RGBA' ! \
                nvvidconv ! 'video/x-raw(memory:NVMM), format=NV12' ! \
                nvv4l2h265enc ! h265parse ! \
                splitmuxsink location=./out_%02d.mp4 max-size-time=1800000000000 sync=true
